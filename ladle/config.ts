d2f3748777463be5d74a8279b6bfe89a918bb5cb
ousttrue <ousttrue@gmail.com>
2024-01-22 02:38:54 +0900

    fix config

--- ladle/config.ts
+++
@@ -1,9 +0,0 @@
-import { UserConfig } from "@ladle/react";
-// import process from "node:process";
-
-const config: UserConfig = {
-  // viteConfig: process.cwd() + "/ladle-vite.config.ts",
-  base: "/map_sample/",
-};
-
-export default config;
